2c2b52e244a745d35a50aca0efaf392facb65eb3
Liwt <19781209@qq.com>
2025-10-12 23:21:05 +0800

    字段显示

--- EIMSNext/admin/src/views/form/components/DataField.vue
+++ EIMSNext/admin/src/views/form/components/DataField.vue
@@ -1,1 +1,55 @@
-<template></template>
+<template>
+    <div>
+        <et-list v-model="value" :data="fieldList" :selectable="true"></et-list>
+        <div class="actions">
+            <el-button type="primary" @click="onOk">确定</el-button>
+            <el-button @click="onCancel">取消</el-button>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { IFormFieldDef, buildFieldListItems } from "@/components/FieldList/type";
+import { IListItem } from "@eimsnext/components";
+import { useFormStore } from "@eimsnext/store";
+
+
+defineOptions({
+    name: "DataField",
+});
+const props = defineProps<{
+    formId: string;
+    modelValue: IFormFieldDef[];
+}>();
+
+const formStore = useFormStore();
+const fieldList = ref<IListItem[]>([]);
+const value = ref(props.modelValue.map(x => x.field));
+
+const emit = defineEmits(["update:modelValue", "ok", "cancel"]);
+const onOk = () => {
+    let listItem = fieldList.value.filter((x) => value.value.indexOf(x.id) > -1).map(x => x.data);
+    emit("update:modelValue", listItem);
+    emit("ok", listItem);
+};
+
+const onCancel = () => {
+    emit("cancel");
+};
+
+onBeforeMount(() => {
+    formStore.get(props.formId).then((form) => {
+        if (form?.content?.items)
+            fieldList.value = buildFieldListItems(form.id, form.content.items, form.usingWorkflow);
+    });
+})
+</script>
+<style lang="scss" scoped>
+.actions {
+    //   border-top: solid 1px #ddd;
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 5px;
+    padding-top: 5px;
+}
+</style>
--- EIMSNext/admin/src/views/form/type.ts
+++ EIMSNext/admin/src/views/form/type.ts
@@ -1,4 +1,6 @@
-import { FieldDef, FieldType } from "@eimsnext/models";
+import { IFormFieldDef } from "@/components/FieldList/type";
+import { FieldDef, FieldType, SystemField } from "@eimsnext/models";
+import { Dictionary } from "@eimsnext/utils";
 
 export interface ITableColumn {
   field: string;
@@ -10,11 +12,36 @@ export interface ITableColumn {
   mergeField?: string;
   oriField: string;
 }
-export function buildColumns(fields: FieldDef[], usingWf: boolean): ITableColumn[] {
+export function buildColumns(
+  fields: FieldDef[],
+  usingWf: boolean,
+  displayFields: IFormFieldDef[]
+): ITableColumn[] {
+  const dispalyAll = displayFields.length == 0;
+  const subDisplayFields = new Dictionary();
+  displayFields.forEach((d) => {
+    if (d.isSubField) {
+      let subFields = d.field.split(">");
+      let subField: IFormFieldDef = {
+        field: subFields[1],
+        formId: d.formId,
+        label: d.label,
+        type: d.type,
+      };
+
+      let mainField = subDisplayFields.get(subFields[0]);
+      if (mainField) {
+        mainField.push(subField);
+      } else {
+        subDisplayFields.add(subFields[0], [subField]);
+      }
+    }
+  });
+
   const columns: ITableColumn[] = [];
-  if (usingWf) {
+  if (usingWf && (dispalyAll || displayFields.find((d) => d.field == SystemField.FlowStatus))) {
     columns.push({
-      field: "flowStatus",
+      field: SystemField.FlowStatus,
       title: "流程状态",
       type: "input",
       mergeField: "_id",
@@ -23,54 +50,70 @@ export function buildColumns(fields: FieldDef[], usingWf: boolean): ITableColumn
   }
 
   fields.forEach((x) => {
-    let col: ITableColumn = {
-      field: x.field,
-      title: x.title,
-      type: x.type,
-      format: x.options?.format,
-      mergeField: "_id",
-      oriField: `data.${x.field}`,
-    };
-    if (x.columns && x.columns.length > 0) {
-      delete col["mergeField"];
-      col.children = buildSubColumns(col.field, x.columns);
-    } else {
-      col.width = 120;
+    if (
+      dispalyAll ||
+      displayFields.find((d) => d.field == x.field) ||
+      subDisplayFields.has(x.field)
+    ) {
+      let col: ITableColumn = {
+        field: x.field,
+        title: x.title,
+        type: x.type,
+        format: x.options?.format,
+        mergeField: "_id",
+        oriField: `data.${x.field}`,
+      };
+      if (x.columns && x.columns.length > 0) {
+        delete col["mergeField"];
+        col.children = buildSubColumns(col.field, x.columns, subDisplayFields.get(x.field));
+      } else {
+        col.width = 120;
+      }
+
+      columns.push(col);
     }
-
-    columns.push(col);
   });
 
-  columns.push({
-    field: "createTime",
-    title: "提交时间",
-    type: "datetime",
-    format: "yyyy-MM-dd",
-    mergeField: "_id",
-    oriField: "createTime",
-  });
+  if (usingWf && (dispalyAll || displayFields.find((d) => d.field == SystemField.CreateTime))) {
+    columns.push({
+      field: SystemField.CreateTime,
+      title: "提交时间",
+      type: "datetime",
+      format: "yyyy-MM-dd",
+      mergeField: "_id",
+      oriField: "createTime",
+    });
+  }
 
   return columns;
 }
 
-function buildSubColumns(pField: string, fields: FieldDef[]): ITableColumn[] {
+function buildSubColumns(
+  pField: string,
+  fields: FieldDef[],
+  subDisplayFields?: IFormFieldDef[]
+): ITableColumn[] {
   const columns: ITableColumn[] = [];
-  fields.forEach((x) => {
-    let col: ITableColumn = {
-      field: x.field,
-      title: x.title,
-      type: x.type,
-      format: x.options?.format,
-      oriField: `${pField}>${x.field}`,
-    };
-    if (x.columns && x.columns.length > 0) {
-      col.children = buildSubColumns(col.field, x.columns);
-    } else {
-      col.width = 120;
-    }
+  if (subDisplayFields) {
+    fields.forEach((x) => {
+      if (subDisplayFields.find((d) => d.field == x.field)) {
+        let col: ITableColumn = {
+          field: x.field,
+          title: x.title,
+          type: x.type,
+          format: x.options?.format,
+          oriField: `${pField}>${x.field}`,
+        };
+        if (x.columns && x.columns.length > 0) {
+          col.children = buildSubColumns(col.field, x.columns);
+        } else {
+          col.width = 120;
+        }
 
-    columns.push(col);
-  });
+        columns.push(col);
+      }
+    });
+  }
 
   return columns;
 }
