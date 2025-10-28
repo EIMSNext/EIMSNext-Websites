e18b52002e865a1a9c65bca614ecac364aa724bb
Liwt <19781209@qq.com>
2025-10-28 21:50:35 +0800

    add new service methods

--- EIMSNext/packages/services/interface/apiServiceBase.ts
+++ EIMSNext/packages/services/interface/apiServiceBase.ts
@@ -5,6 +5,16 @@ import { ServiceBase } from "./serviceBase";
 export abstract class ApiServiceBase<T = IdBase, R = any> extends ServiceBase {
   protected abstract modelName(): string;
 
+  dynamiccount(query?: any): Promise<number> {
+    let url = this.getUrl(this.modelName(), "$dynamiccount");
+    return this.http().api.count(url, query);
+  }
+
+  dynamicquery<T>(query?: any): Promise<T[]> {
+    let url = this.getUrl(this.modelName(), "$dynamicquery");
+    return this.http().api.query(url, query);
+  }
+
   get<T>(id: string, query?: any): Promise<T> {
     let url = this.getUrl(this.modelName(), id);
     return this.http().api.get<T>(url, query);
