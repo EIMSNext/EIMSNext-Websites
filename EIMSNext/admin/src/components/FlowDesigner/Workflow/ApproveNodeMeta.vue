8c7339c2bffeeaa42af562edbf21e0396881e6e1
Liwt <19781209@qq.com>
2025-11-23 17:19:32 +0800

    fix workflow issue

--- EIMSNext/admin/src/components/FlowDesigner/Workflow/ApproveNodeMeta.vue
+++ EIMSNext/admin/src/components/FlowDesigner/Workflow/ApproveNodeMeta.vue
@@ -1,30 +1,16 @@
 <template>
-  <MetaItemHeader :label="t('节点负责人')" :required="true" tips="最多选择100人"></MetaItemHeader>
-  <el-select
-    v-model="activeData.metadata.approveMeta!.approveMode"
-    class="sub-item"
-    style="width: 100%; margin-bottom: 8px"
-  >
-    <el-option label="或签" :value="1" />
-    <el-option label="会签" :value="2" />
-  </el-select>
-  <selected-tags
-    v-model="selectedCandidateTags"
-    :editable="true"
-    :empty-text="'选择成员或部门'"
-    @editTag="editTag"
-  />
-  <el-checkbox
-    v-model="activeData.metadata.approveMeta!.enableCopyto"
-    label="启用抄送"
-    class="sub-item"
-  />
-  <member-select-dialog
-    v-model="showMemberDialog"
-    :tags="selectedCandidateTags"
-    destroy-on-close
-    @ok="finishSelect"
-  />
+  <template v-if="ready">
+    <MetaItemHeader :label="t('节点负责人')" :required="true" tips="最多选择100人"></MetaItemHeader>
+    <el-select v-model="activeData.metadata.approveMeta!.approveMode" class="sub-item"
+      style="width: 100%; margin-bottom: 8px">
+      <el-option label="或签" :value="1" />
+      <el-option label="会签" :value="2" />
+    </el-select>
+    <selected-tags v-model="selectedCandidateTags" :editable="true" :empty-text="'选择成员或部门'" @editTag="editTag" />
+    <el-checkbox v-model="activeData.metadata.approveMeta!.enableCopyto" label="启用抄送" class="sub-item" />
+    <member-select-dialog v-model="showMemberDialog" :tags="selectedCandidateTags" destroy-on-close
+      @ok="finishSelect" />
+  </template>
 </template>
 <script lang="ts" setup>
 import {
@@ -47,6 +33,7 @@ defineOptions({
   name: "ApproveNodeMeta",
 });
 
+const ready = ref(false)
 const flowContext = inject<IFlowContext>("flowContext");
 const flowContextRef = reactive<IFlowContext>(flowContext!);
 const activeData = ref<IFlowNodeData>(createFlowNode(FlowNodeType.None));
@@ -58,7 +45,7 @@ const editTag = () => {
 };
 const finishSelect = (tags: ISelectedTag[]) => {
   //   console.log("sel tags", tags);
-  selectedCandidateTags.value = tags;
+  // selectedCandidateTags.value = tags;
   let candidate: IApprovalCandidate[] = [];
   tags.forEach((x) => candidate.push(convertTagToCandidate(x)));
   activeData.value.metadata.approveMeta!.approvalCandidates = candidate;
@@ -66,11 +53,9 @@ const finishSelect = (tags: ISelectedTag[]) => {
   showMemberDialog.value = false;
 };
 
-watch(
-  flowContextRef,
-  (newValue: IFlowContext) => {
-    // console.log("activeData", newValue.activeData.metadata);
-    activeData.value = newValue.activeData;
+const init = () => {
+  nextTick(async () => {
+    activeData.value = flowContextRef.activeData;
 
     selectedCandidateTags.value = [];
     if (activeData.value.metadata.approveMeta!.approvalCandidates) {
@@ -80,7 +65,28 @@ watch(
       );
       selectedCandidateTags.value = tags;
     }
-  },
-  { immediate: true }
-);
+
+    ready.value = true
+  })
+}
+
+init()
+
+// watch(
+//   flowContextRef,
+//   (newValue: IFlowContext) => {
+//     // console.log("activeData", newValue.activeData.metadata);
+//     activeData.value = newValue.activeData;
+
+//     selectedCandidateTags.value = [];
+//     if (activeData.value.metadata.approveMeta!.approvalCandidates) {
+//       let tags: ISelectedTag[] = [];
+//       activeData.value.metadata.approveMeta!.approvalCandidates.forEach((x: IApprovalCandidate) =>
+//         tags.push(convertCandidateToTag(x))
+//       );
+//       selectedCandidateTags.value = tags;
+//     }
+//   },
+//   { immediate: true }
+// );
 </script>
