9bfc15e157c4482e41ae4fb8fa2f1a319250c97e
Liwt <19781209@qq.com>
2025-12-08 18:31:20 +0800

    add exm:vant

--- EIMSNext/examples/vant/src/main.js
+++ EIMSNext/examples/vant/src/main.js
@@ -3,8 +3,23 @@ import FormCreateMobile from "@eimsnext/form-render-vant";
 import install from "@eimsnext/form-render-vant/auto-import";
 import "vant/lib/index.css";
 import App from "./App.vue";
+import { appSetting, http, HttpInterceptors } from "@eimsnext/utils";
+import { AxiosHeaders } from "axios";
 
 const app = createApp(App);
+//@ts-ignore
+if (window.appSetting) appSetting.merge(window.appSetting);
+let interceptors = new HttpInterceptors();
+interceptors.errorHandler = (msg) => {
+  alert(msg);
+};
+let httpConfig = {
+  headers: new AxiosHeaders(),
+  interceptors,
+};
+http.setConfig(httpConfig, httpConfig);
+
+console.log("http", http);
 
 FormCreateMobile.use(install);
 
