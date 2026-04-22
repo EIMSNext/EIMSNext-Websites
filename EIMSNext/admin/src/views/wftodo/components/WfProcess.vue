024b8f127eb8ae1365bc751c5204dfa763af60c2
Liwt <19781209@qq.com>
2026-04-22 12:54:00 +0800

    审批意见

--- EIMSNext/admin/src/views/wftodo/components/WfProcess.vue
+++ EIMSNext/admin/src/views/wftodo/components/WfProcess.vue
@@ -1,6 +1,11 @@
 <template>
   <FormView v-if="formData" :def="formDef" :data="formData" :isView="true" :actions="actions" @approve="handleApprove"
     @reject="handleReject"></FormView>
+
+  <et-dialog v-model="showCommentDialog" title="审批意见" width="500px" :append-to-body="true" :destroy-on-close="true"
+    @ok="confirmApproveAction" @cancel="cancelApproveAction">
+    <el-input v-model="comment" type="textarea" :rows="4" placeholder="请输入审批意见" />
+  </et-dialog>
 </template>
 <script lang="ts" setup>
 defineOptions({
@@ -35,6 +40,9 @@ const dataId = ref(props.todo.dataId);
 const formStore = useFormStore();
 const formDef = ref<FormContent>(new FormContent());
 const formData = ref<FormData_2>();
+const showCommentDialog = ref(false);
+const comment = ref("");
+const pendingAction = ref<ApproveAction>();
 
 const emit = defineEmits(["update:modelValue", "cancel", "submit"]);
 const cancel = () => {
@@ -42,19 +50,43 @@ const cancel = () => {
   emit("cancel");
 };
 
-const handleApprove = (data: any) => {
-  workflowService.approve({
-    dataId: dataId.value,
-    action: ApproveAction.Approve,
-    comment: "同意",
-  }).catch();
+const resetCommentDialog = () => {
+  showCommentDialog.value = false;
+  comment.value = "";
+  pendingAction.value = undefined;
 };
-const handleReject = (data: any) => {
-  workflowService.approve({
-    dataId: dataId.value,
-    action: ApproveAction.Reject,
-    comment: "不同意",
-  }).catch();
+
+const handleApprove = () => {
+  pendingAction.value = ApproveAction.Approve;
+  comment.value = "";
+  showCommentDialog.value = true;
+};
+
+const handleReject = () => {
+  pendingAction.value = ApproveAction.Reject;
+  comment.value = "";
+  showCommentDialog.value = true;
+};
+
+const confirmApproveAction = async () => {
+  if (!pendingAction.value) {
+    return;
+  }
+
+  try {
+    await workflowService.approve({
+      dataId: dataId.value,
+      action: pendingAction.value,
+      comment: comment.value,
+    });
+    resetCommentDialog();
+  }
+  catch {
+  }
+};
+
+const cancelApproveAction = () => {
+  resetCommentDialog();
 };
 
 onMounted(async () => {
