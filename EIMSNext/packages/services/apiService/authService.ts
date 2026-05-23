c169256a0182fbb2b9854512901101c14cb810e5
Liwt <19781209@qq.com>
2026-05-23 18:01:13 +0800

    单机版没有集成登录

--- EIMSNext/packages/services/apiService/authService.ts
+++ EIMSNext/packages/services/apiService/authService.ts
@@ -1,32 +1,9 @@
 import { LoginRequest } from "../requestModel";
 import { ServiceBase } from "../interface";
 
-export interface IntegrationAuthorizationUrlDto {
-  type?: string;
-  displayName?: string;
-  enabled?: boolean;
-  authorizationUrl?: string;
-}
-
 export class AuthService extends ServiceBase {
   login(request: LoginRequest) {
     return this.http().auth.login(request.username, request.password, request.grant_type);
-  }
-
-  async getIntegrationAuthorizationUrl(type: string, state: string): Promise<IntegrationAuthorizationUrlDto> {
-    const result = await this.http().auth.get<IntegrationAuthorizationUrlDto & {
-      AuthorizationUrl?: string;
-      DisplayName?: string;
-      Enabled?: boolean;
-      Type?: string;
-    }>("/open/integration/authorize", { type, state }, false);
-
-    return {
-      type: result.type ?? result.Type,
-      displayName: result.displayName ?? result.DisplayName,
-      enabled: result.enabled ?? result.Enabled,
-      authorizationUrl: result.authorizationUrl ?? result.AuthorizationUrl,
-    };
   }
 }
 
