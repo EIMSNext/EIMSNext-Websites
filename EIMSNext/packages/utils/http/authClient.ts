660e84a619adcaff87842d00a69dc894eae9a949
Liwt <19781209@qq.com>
2026-06-15 14:00:46 +0800

    refactor

--- EIMSNext/packages/utils/http/authClient.ts
+++ EIMSNext/packages/utils/http/authClient.ts
@@ -4,6 +4,8 @@ import { HttpRequest } from "./httpRequest";
 import { ContentType } from "./interface";
 import accessToken from "./token";
 
+const utf8ToBase64 = (s: string) => btoa(unescape(encodeURIComponent(s)));
+
 export class AuthClient {
   private httpRequest: HttpRequest;
   constructor(request: HttpRequest) {
@@ -11,18 +13,19 @@ export class AuthClient {
   }
 
   login(username: string, password: string, grant_type = "password") {
-    let params = {
+    const payload = {
       username,
       password,
       grant_type,
       client_id: appSetting.clientId,
     };
-    let url = `${appSetting.authUrl}/connect/token`;
-    let headers = new AxiosHeaders();
+    const encrypted = utf8ToBase64(JSON.stringify(payload));
+    const url = `${appSetting.authUrl}/auth/login`;
+    const headers = new AxiosHeaders();
     headers.setContentType(ContentType.FORM_URLENCODED);
     return new Promise((resolve, reject) => {
       this.httpRequest
-        .post({ url, data: params, headers, withToken: false })
+        .post({ url, data: { encrypted }, headers, withToken: false })
         .then((res) => {
           accessToken.set(res.access_token, res.expires_in);
           resolve(res);
